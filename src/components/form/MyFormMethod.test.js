d01a5dbe42db643def44e7b9bc9abb1644c00a7d
José Ricardo Xavier Cavalcanti Júnior <75625584+jrxcjr@users.noreply.github.com>
2021-04-02 00:11:59 -0300

    tried to implement tests

---
+++ src/components/form/MyFormMethod.test.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import{render,screen, fireEvent,act, getByDisplayValue, getByPlaceholderText} from '@testing-library/react'
+import { MyFormFields } from './MyFormFields'; 
+import handleSubmit from './MyFormMethods'
+import SelectTypeComponent from './SelectTypeComponent';
+
+
+const SuccessfulElements = {
+    "callDestination": "011",
+    "callOrigin": "016",
+    "faleMaisPlan": "falemais60",
+    "minutes": "200"
+}
+const SuccessfulElementsOutput = {
+    callDestination: "011",
+    callOrigin: "016",
+    faleMaisPlan: "falemais60",
+    minutes: "200"
+}
+
+const InvalidCombination = {
+"callDestination": "011",
+"callOrigin": "016",
+"faleMaisPlan": "falemais60",
+"minutes": "200"}
+
+
+describe('submit button', () => {
+    // previous tests
+  
+    it('calls handleSubmit with the correct values', () => {
+  
+      render(
+        <MyFormFields onSubmit={handleSubmit} />
+      );
+  
+      fireEvent.change(<SelectTypeComponent/>, {
+        target: { value: '011' }
+      });
+  
+      fireEvent.click(getByDisplayValue('Send the Data!'));
+  
+      expect(handleSubmit).toHaveBeenCalledWith({
+        callOrigin:"011"
+      });
+    });
+  });
+
+//wasn't able to finish the implementation of the test function handleSubmit on time, wasn't able
+//to properly pass the mock values to make the function execute as it does when the user is calling it.
--- src/components/form/TableDisplayResults.js
+++ src/components/form/TableDisplayResults.js
@@ -5,21 +5,21 @@ function TableDisplayResults(props) {
  return (
   <ListGroup>
    <ListGroup.Item>
-    Your call originated From DDD: {props.FirstGroup}
+    Your call originated From DDD: <strong>{props.FirstGroup}</strong>
    </ListGroup.Item>
    <ListGroup.Item>
-    Your Call had the Destination to DDD: {props.SecondGroup}
+    Your Call had the Destination to DDD: <strong>{props.SecondGroup}</strong>
    </ListGroup.Item>
-   <ListGroup.Item>Your Call had {props.ThirdGroup} Minutes </ListGroup.Item>
+   <ListGroup.Item>Your Call had <strong>{props.ThirdGroup}</strong> Minutes </ListGroup.Item>
    <ListGroup.Item>
-    You have the Fale mais Plan: {props.FourthGroup}
-   </ListGroup.Item>
-   <ListGroup.Item>
-    Without a plan your call price was ${props.FifthGroup}
+    You have the Fale mais Plan: <strong>{props.FourthGroup}</strong>
    </ListGroup.Item>
    <ListGroup.Item>
-    Your call price was ${props.SixthGroup} because you have a plan, and your
-    plan is "{props.SeventhGroup}"
+    Without a plan your call price would be $<strong>{props.FifthGroup}</strong>
+   </ListGroup.Item>
+   <ListGroup.Item>
+    Your call price was $<strong>{props.SixthGroup}</strong> because you have a plan, and your
+    plan is "<strong>{props.SeventhGroup}</strong>"
    </ListGroup.Item>
   </ListGroup>
  );
